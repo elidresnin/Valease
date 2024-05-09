e54f731a0ba67d75e65b81e4454afa5e85f84e4e
Penny Cohen <rosepenny2007@gmail.com>
2024-05-09 11:54:42 -0400

    implemented places views into app

--- Valease/Views/Places/PlacesDetailView.swift
+++ Valease/Views/Places/PlacesDetailView.swift
@@ -11,8 +11,11 @@ import MapKit
 struct PlacesDetailView: View {
     @EnvironmentObject var places: PlaceData
     @State var place: Place
+    @EnvironmentObject var events : Events
+    @State private var isShowingPicker = false
     
     var body: some View {
+
         VStack {
             AsyncImage(
                 url: URL(string: "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photo_reference=\(place.photo)&key=AIzaSyDNMYZ6l67iAy_HCSjAAl6Ljrj1oVLWseY"),
@@ -29,39 +32,62 @@ struct PlacesDetailView: View {
                     
                 }
             )
-            Spacer()
-            Text(place.name)
-                .font(Constants.mediumFont)
-                .bold()
-                .multilineTextAlignment(.center)
-                .padding()
-            Text(place.address)
-                .font(Constants.mediumFont)
-                .multilineTextAlignment(.center)
-                .padding()
-            Text(place.business_status)
-                .multilineTextAlignment(.center)
-                .padding()
-            Spacer()
-            if let _ = place.price_level{
-                Text("Price Level: \(String(place.price_level ?? 0))/4")
+            ScrollView{
+                
+                Text(place.name)
+                    .font(Constants.mediumFont)
+                    .bold()
                     .multilineTextAlignment(.center)
                     .padding()
-            }
+                Text(place.address)
+                    .font(Constants.mediumFont)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text(place.business_status)
+                    .multilineTextAlignment(.center)
+                    .padding()
                 
-            Spacer()
-            HStack{
-                ForEach(0..<Int(place.rating)) {_ in
-                       return Image(systemName: "star.fill")
-                        .foregroundColor(Color.valeaseOrange)
+                if let _ = place.price_level{
+                    Text("Price Level: \(String(place.price_level ?? 0))/4")
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
+                
+                
+                HStack{
+                    ForEach(0..<Int(place.rating)) {_ in
+                        return Image(systemName: "star.fill")
+                            .foregroundColor(Color.valeaseOrange)
                     }
-                ForEach(Int(place.rating)..<5) {_ in
-                       return Image(systemName: "star")
-                        .foregroundColor(Color.valeaseOrange)
+                    ForEach(Int(place.rating)..<5) {_ in
+                        return Image(systemName: "star")
+                            .foregroundColor(Color.valeaseOrange)
                     }
+                }
+                
+                DatePicker("Select a date", selection: $place.date, in: Date()..., displayedComponents: [.hourAndMinute, .date])
+                    .padding()
+                
+                
+                Button {
+                    
+                    let newPlace = Event(name: place.name, location: place.address, date: place.date)
+                    
+                    events.eventList.append(newPlace)
+                    
+                } label: {
+                    Text("Add to itinerary")
+                        .padding()
+                        .background(Color.valeaseTeal)
+                        .cornerRadius(30)
+                    
+                    
+                }
+                
+                
+                
             }
-            
-            Spacer()
+           
         }
     }
 }
@@ -70,5 +96,6 @@ struct PlacesDetailView_Previews: PreviewProvider {
     static var previews: some View {
         PlacesDetailView(place: Place())
             .environmentObject(PlaceData())
+            .environmentObject(Events())
     }
 }
--- Valease/Views/Places/PlacesView.swift
+++ Valease/Views/Places/PlacesView.swift
@@ -40,6 +40,7 @@ struct PlacesView: View {
             }
             
         }
+       
     }
     
     
